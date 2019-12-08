d84b9bcc0407632f8600696d74713cf458cdea41
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 07:31:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
   
+  data.each { |key, value|
+    value.each { |value2, name|
+      name.each { |name|
+        if !result[name]
+          result[name] = {}
+        end
+        
+        if !result[name][key]
+          result[name] = {}
+        end
+        
+        result[name][key] << (value2.to_s)
+        
+      }
+    }
+  }
+  
+  result
 end
