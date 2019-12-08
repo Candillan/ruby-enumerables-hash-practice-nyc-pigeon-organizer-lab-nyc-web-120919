89efd5848e25915c0bd8566b451da73e83bbac28
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 07:30:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
           result[name] = {}
         end
         
-        result[name][key].push(value2.to_s)
+        result[name][key] << (value2.to_s)
         
       }
     }
