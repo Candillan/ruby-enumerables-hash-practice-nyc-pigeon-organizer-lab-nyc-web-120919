30929ebdc45278b82d95e70802819955508af3fd
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 07:29:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,28 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   result = {}
+  
   data.each { |key, value|
-    
-end
-
-def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
-      names.each do |name|
+    value.each { |value2, name|
+      name.each { |name|
+        if !result[name]
+          result[name] = {}
+        end
         
-        if !new_hash[name]
-          new_hash[name] = {}
+        if !result[name][key]
+          result[name] = {}
         end
-
-        if !new_hash[name][key]
-          new_hash[name][key] = []
-        end
-
-        new_hash[name][key] << new_value.to_s
-
+        
+        result[name][key].push(value2.to_str)
+        
       end
     end
   end
-  new_hash
+  
+  result
 end
